182360877798b9329766c7797136bbdcc26e2fdd
Fatema04 <shadrinmoni15@gmail.com>
2022-10-05 11:34:14 -0700

    2nd mtd of  router add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Home from './components/Home/Home';
 
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <div>This is default page</div> },
-    { path: "/home", element: <div>This is home page</div> },
-    { path: "/about", element: <div>This is about page</div> },
+    { path: "/home", element: <Home></Home> },
+    { path: "/about", element: <About></About> },
 
   ])
   return (
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+
+            <h3>This is real home page</h3>
+
+        </div>
+    );
+};
+
+export default Home;
