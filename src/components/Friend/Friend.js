47fce7a4f93d7c9695b3729561a9ae36f666b484
Fatema04 <shadrinmoni15@gmail.com>
2022-10-05 19:45:39 -0700

    diff router 4 diff frnd

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Friend = ({ friend }) => {
-    const { name, email } = friend
+    const { id, name, email, username } = friend
     return (
         <div>
 
             <h3>Name : {name}</h3>
             <p>Email : {email}</p>
+            <small>username :<Link to={`/friend/${id}`}>{username}</Link></small>
 
 
         </div>
